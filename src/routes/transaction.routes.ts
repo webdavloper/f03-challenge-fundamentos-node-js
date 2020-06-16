4185642f27a1dd323c6a68b959a4241ee29d7bd3
David Barbosa <davidsilva.sp@outlook.com>
2020-06-15 21:26:48 -0300

    update transactions.routes.ts

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -34,7 +34,7 @@ transactionRouter.post('/', (request, response) => {
       type,
     });
 
-    return response.json({ transaction });
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
